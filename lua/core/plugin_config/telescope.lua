373139807ba1dddebc73e186a885ce51431af037
Gabi Chiosa <gabi@bittreatsoftware.com>
2024-03-02 06:54:44 +0200

    added ios starter kit

--- lua/core/plugin_config/telescope.lua
+++
@@ -1,7 +0,0 @@
-require('telescope').setup({ file_ignore_patterns = { "node%_modules/.*" } })
-local builtin = require('telescope.builtin')
-
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
---
+++ lua/starter/plugins/nvim-lspconfig.lua
@@ -0,0 +1,47 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    { "antosha417/nvim-lsp-file-operations", config = true },
+  },
+  config = function()
+    local lspconfig = require("lspconfig")
+    local util = require("lspconfig.util")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+    local opts = { noremap = true, silent = true }
+    local on_attach = function(_, bufnr)
+      opts.buffer = bufnr
+
+      opts.desc = "Show line diagnostics"
+      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+
+      opts.desc = "Show documentation for what is under cursor"
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    end
+
+    lspconfig["sourcekit"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      cmd = {
+        vim.trim(vim.fn.system("xcrun -f sourcekit-lsp")),
+        -- TODO: in case of issues try using the full path to sourcekit-lsp:
+        -- "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp",
+      },
+      root_dir = function(filename, _)
+        return util.root_pattern("buildServer.json")(filename)
+          or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
+          or util.find_git_ancestor(filename)
+          or util.root_pattern("Package.swift")(filename)
+      end,
+    })
+
+    -- nice icons
+    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
+  end,
+}
